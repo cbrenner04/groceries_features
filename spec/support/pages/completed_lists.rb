c16983a17456082e411c82d0e1e6a7d4e2853038
Christopher Brenner <cbrenner265@gmail.com>
2019-10-26 18:41:03 -0500

    Completed list fix take 2 (#27)
    * Add more tests for completed lists
    
    * Lint

--- spec/support/pages/completed_lists.rb
+++ spec/support/pages/completed_lists.rb
@@ -7,6 +7,12 @@ module Pages
     DELETE_BUTTON = '.fa.fa-trash'
     REFRESH_BUTTON = '.fa.fa-refresh'
 
+    elements :complete_list_names, "#{COMPLETE_LIST} h5"
+
+    def delete(list_name)
+      find_complete_list(list_name).find(DELETE_BUTTON).click
+    end
+
     def delete_button_css
       DELETE_BUTTON
     end
@@ -17,6 +23,10 @@ module Pages
 
     def refresh_button_css
       REFRESH_BUTTON
+    end
+
+    def refresh(list_name)
+      find_complete_list(list_name).find(REFRESH_BUTTON).click
     end
   end
 end
