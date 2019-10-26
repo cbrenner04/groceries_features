f33679f32118c822888939bad44def6d7237999a
cbrenner04 <cbrenner265@gmail.com>
2019-10-26 18:38:34 -0500

    Add more tests for completed lists

--- spec/features/completed_lists.rb
+++ spec/features/completed_lists.rb
@@ -18,7 +18,7 @@ RSpec.feature 'Completed lists page' do
       completed: true,
       owner_id: user.id
     )
-    create_associated_list_objects(user, @list)
+    @list_items = create_associated_list_objects(user, @list)
     @other_list = Models::List.new(
       type: list_type,
       owner_id: other_user.id,
@@ -27,16 +27,52 @@ RSpec.feature 'Completed lists page' do
     create_associated_list_objects(user, @other_list)
 
     login user
+    home_page.go_to_completed_lists
   end
 
-  it 'shows correct shit' do
-    home_page.go_to_completed_lists
-    owned_list = completed_lists_page.find_complete_list(@list.name)
-    shared_list = completed_lists_page.find_complete_list(@other_list.name)
+  it 'refreshes list' do
+    completed_lists_page.refresh @list.name
 
-    expect(owned_list).to have_css completed_lists_page.refresh_button_css
-    expect(owned_list).to have_css completed_lists_page.delete_button_css
-    expect(shared_list).to have_no_css completed_lists_page.refresh_button_css
-    expect(shared_list).to have_no_css completed_lists_page.delete_button_css
+    wait_for do
+      home_page.incomplete_list_names.map(&:text).include? @list.name
+    end
+
+    expect(home_page.incomplete_list_names.map(&:text)).to include @list.name
+
+    home_page.select_list @list.name
+
+    expect(list_page).to have_not_purchased_items
+    expect(list_page.not_purchased_items.map(&:text))
+      .to include @list_items.first.pretty_title
+    expect(list_page.not_purchased_items.map(&:text))
+      .to include @list_items.last.pretty_title
+
+  end
+
+  it 'deletes list' do
+    wait_for do
+      completed_lists_page.complete_list_names.map(&:text).include? @list.name
+    end
+
+    home_page.accept_alert do
+      home_page.delete @list.name, complete: true
+    end
+
+    wait_for do
+      !home_page.complete_list_names.map(&:text).include?(@list.name)
+    end
+
+    expect(home_page.complete_list_names.map(&:text)).to_not include @list.name
+
+  end
+
+
+  describe 'shared list' do
+    it 'does not show refresh or delete' do
+      shared_list = completed_lists_page.find_complete_list(@other_list.name)
+
+      expect(shared_list).to have_no_css completed_lists_page.refresh_button_css
+      expect(shared_list).to have_no_css completed_lists_page.delete_button_css
+    end
   end
 end
