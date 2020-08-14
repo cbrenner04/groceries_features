cb81acd33f4a5c08b94576094443c56ab095aa5d
cbrenner04 <cbrenner265@gmail.com>
2020-08-14 12:46:35 -0500

    Fix auth for results

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# override the number of parallels by setting PARALLELS. otherwise, defaults to 5
+: ${PARALLELS:=5}
+
+# sign in only once, before all tests start
 ruby spec/sign_in_to_results.rb
-bundle exec parallel_rspec spec/ -n 7 --serialize-stdout --combine-stderr --group-by runtime
+# run tests
+PARALLELS=$PARALLELS bundle exec parallel_rspec spec/ -n $PARALLELS --serialize-stdout --combine-stderr --group-by runtime
+# sign out only once, after all tests are finished
 ruby spec/sign_out_of_results.rb
+# clean up the database only once, after all tests are finished
 ruby spec/clean_database.rb
