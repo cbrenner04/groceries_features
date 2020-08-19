e9247f59407fd3e14932cb33868e8d43ea0c8fdf
Christopher Brenner <cbrenner265@gmail.com>
2020-08-19 10:39:08 -0500

    Updates for multiselect split between complete and incomplete list items (#67)
    * Updates for multiselect split between complete and incomplete list items
    
    * Tell rubocop to shove it
    
    * Correct parallel runtime log

--- spec/features/list_items/simple_list_items_spec.rb
+++ spec/features/list_items/simple_list_items_spec.rb
@@ -26,42 +26,7 @@ RSpec.describe "A simple list item", type: :feature do
   end
 
   it_behaves_like "a list item", "content", "SimpleList", Models::SimpleListItem, []
-
-  describe "when logged in as owner" do
-    before do
-      login user
-      list_page.load(id: list.id)
-      @initial_list_item_count = list_page.not_purchased_items.count
-    end
-
-    describe "that is completed" do
-      it "is refreshed" do
-        item_name = @list_items.last.pretty_title
-
-        list_page.refresh item_name
-
-        wait_for { list_page.not_purchased_items.count == @initial_list_item_count + 1 }
-
-        list_page.filter_button.click
-        list_page.filter_option("foo").click
-
-        expect(list_page.not_purchased_items.map(&:text)).to include item_name
-      end
-    end
-
-    describe "when multiple selected" do
-      it "is refreshed" do
-        list_page.multi_select_button.click
-        @list_items.each { |item| list_page.multi_select_item(item.pretty_title, purchased: item.completed) }
-        list_page.refresh(@list_items.last.pretty_title)
-
-        wait_for { list_page.not_purchased_items.count == 0 }
-
-        expect(list_page.purchased_items.count).to eq 0
-        expect(list_page.not_purchased_items.count).to eq 3
-      end
-    end
-  end
+  it_behaves_like "a refreshable list item", "SimpleList"
 
   describe "when logged in as shared user with write access" do
     before do
@@ -78,7 +43,7 @@ RSpec.describe "A simple list item", type: :feature do
       list_page.expand_list_item_form
       expect(list_page).to have_content_input
       expect(list_page).to have_submit_button
-      expect(list_page).to have_multi_select_button
+      expect(list_page).to have_multi_select_buttons
       expect(not_purchased_item).to have_css list_page.purchase_button_css
       expect(not_purchased_item).to have_css list_page.edit_button_css
       expect(not_purchased_item).to have_css list_page.delete_button_css
@@ -101,7 +66,7 @@ RSpec.describe "A simple list item", type: :feature do
 
       expect(list_page).to have_no_content_input
       expect(list_page).to have_no_submit_button
-      expect(list_page).to have_no_multi_select_button
+      expect(list_page).to have_no_multi_select_buttons
       expect(not_purchased_item).to have_no_css list_page.purchase_button_css
       expect(not_purchased_item).to have_no_css list_page.edit_button_css
       expect(not_purchased_item).to have_no_css list_page.delete_button_css
