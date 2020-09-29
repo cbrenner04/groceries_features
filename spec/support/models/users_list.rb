f00331d841ac2d25f0d6895c0b9b20b8aad61c10
cbrenner04 <cbrenner265@gmail.com>
2020-09-29 17:23:26 -0500

    Some updates for drag and drop

--- spec/support/models/users_list.rb
+++ spec/support/models/users_list.rb
@@ -3,20 +3,22 @@
 module Models
   # relates a user and a list
   class UsersList
-    attr_reader :id, :user_id, :list_id, :has_accepted, :permissions
+    attr_reader :id, :user_id, :list_id, :has_accepted, :permissions, :rank
 
     def initialize(user_id:, list_id:, has_accepted: true, permissions: "write", create_list: true)
       @user_id = user_id
       @list_id = list_id
       @has_accepted = has_accepted
       @permissions = permissions
+      @rank = 1
       @id = create if create_list
     end
 
     private
 
     def create
-      DB[:users_lists].insert(user_id: user_id, list_id: list_id, has_accepted: has_accepted, permissions: permissions)
+      DB[:users_lists]
+        .insert(user_id: user_id, list_id: list_id, has_accepted: has_accepted, permissions: permissions, rank: rank)
     end
   end
 end
