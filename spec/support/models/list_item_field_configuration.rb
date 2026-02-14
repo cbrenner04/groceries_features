85633e5f4cb0d0de167115ff776c3c71f1afafc7
cbrenner04 <cbrenner265@gmail.com>
2026-02-14 11:04:38 -0600

    Lint

--- spec/support/models/list_item_field_configuration.rb
+++ spec/support/models/list_item_field_configuration.rb
@@ -6,7 +6,8 @@ module Models
     attr_accessor :label, :data_type, :archived_at, :list_item_configuration_id, :position
     attr_reader :id, :created_at, :updated_at
 
-    def initialize(label:, data_type:, archived_at:, list_item_configuration_id:, position:, create_field: true, primary: false)
+    def initialize(label:, data_type:, archived_at:, list_item_configuration_id:, position:, create_field: true,
+                   primary: false)
       @label = label
       @data_type = data_type
       @archived_at = archived_at
